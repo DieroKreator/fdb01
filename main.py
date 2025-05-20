ac70787e8adc4de8f3a4bfb92db62085c2336646
DieroKreator <qtechwired@gmail.com>
2025-05-20 16:43:33 -0600

    Refactor main menu logic to use match-case for option selection

--- main.py
+++ main.py
@@ -11,22 +11,36 @@ def main():
     print("2 - Subtração de dois números")
 
     opcao = input("Digite o número da opção desejada: ")   
-    if opcao == "1":
-        num1 = int(input("Digite o valor do 1o número: "))
-        num2 = int(input("Digite o valor do 2o número: "))
-        # # jeito mais antigo
-        # soma = num1 + num2
-        # print("Soma =",soma)
 
-        # jeito mais atual
-        print(f"Soma = {num1 + num2}")
-    elif opcao == "2":
-        num1 = int(input("Digite o valor do 1o número: "))
-        num2 = int(input("Digite o valor do 2o número: "))
+    # # Antes do Python versão 3.10
+    # if opcao == "1":
+    #     num1 = int(input("Digite o valor do 1o número: "))
+    #     num2 = int(input("Digite o valor do 2o número: "))
+    #     # # jeito mais antigo
+    #     # soma = num1 + num2
+    #     # print("Soma =",soma)
 
-        print(f"Subtração = {num1 - num2}")
-    else:
-        print("Opção inválida, execute o programa novamente.")
+    #     # jeito mais atual
+    #     print(f"Soma = {num1 + num2}")
+    # elif opcao == "2":
+    #     num1 = int(input("Digite o valor do 1o número: "))
+    #     num2 = int(input("Digite o valor do 2o número: "))
+
+    #     print(f"Subtração = {num1 - num2}")
+    # else:
+    #     print("Opção inválida, execute o programa novamente.")
+
+    match opcao:
+        case "1":
+            num1 = int(input("Digite o valor do 1o número: "))
+            num2 = int(input("Digite o valor do 2o número: "))
+            print(f"Soma = {num1 + num2}")
+        case "2":
+            num1 = int(input("Digite o valor do 1o número: "))
+            num2 = int(input("Digite o valor do 2o número: "))
+            print(f"Subtração = {num1 - num2}")
+        case _:
+            print("Opção inválida, execute o programa novamente.")
 
 if __name__ == "__main__":
     main()
