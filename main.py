8c60eb21c08f3f0b0349b37e71f008d1a44f829d
DieroKreator <qtechwired@gmail.com>
2025-05-20 16:34:30 -0600

    Add subtraction functionality to main menu

--- main.py
+++ main.py
@@ -8,6 +8,8 @@
 def main():
     print("############ MENU PRINCIPAL ############")
     print("1 - Soma de dois números")
+    print("2 - Subtração de dois números")
+
     opcao = input("Digite o número da opção desejada: ")   
     if opcao == "1":
         num1 = int(input("Digite o valor do 1o número: "))
@@ -15,9 +17,16 @@ def main():
         # # jeito mais antigo
         # soma = num1 + num2
         # print("Soma =",soma)
-        
+
         # jeito mais atual
         print(f"Soma = {num1 + num2}")
+    elif opcao == "2":
+        num1 = int(input("Digite o valor do 1o número: "))
+        num2 = int(input("Digite o valor do 2o número: "))
+
+        print(f"Subtração = {num1 - num2}")
+    else:
+        print("Opção inválida, execute o programa novamente.")
 
 if __name__ == "__main__":
     main()
